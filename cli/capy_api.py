64076b160ef16931cddb2655406e47df522d7a8a
MingYuanTina <bjteana@gmail.com>
2024-01-10 05:11:41 -0800

    refactor and add ChatGPT test option

--- cli/capy_api.py
+++ cli/capy_api.py
@@ -9,72 +9,68 @@ class CopilotClient:
     def __init__(self):
         pass
 
-    def make_request_to_bot(self, tf_output, command, question=None):
+    def make_request_to_bot(self, sys_command, sys_command_output, capy_command, question=None):
+        msg_content = "Given the following " + sys_command + ": \n";
+        msg_content += sys_command_output
+        msg_content += "\n"
+
         chatbot = ChatBot()
-        if command == "explain":
-            msg_content = "Given the following terraform plan: \n";
-            msg_content += tf_output
-            msg_content += "\n"
+        if capy_command == "explain":
             msg_content += "Explain what it does"
-            chatbot.make_request(msg_content)
-
-        elif command == "cost":
-            msg_content = "Given the following terraform plan: \n";
-            msg_content += tf_output
-            msg_content += "\n"
+        elif capy_command == "cost":
             msg_content += "Estimate how much it woulld cost"
-            chatbot.make_request(msg_content)
-
-        elif command == "question":
-            msg_content = "Given the following terraform plan: \n";
-            msg_content += tf_output
-            msg_content += "\n"
+        elif capy_command == "question":
             msg_content += question
-            chatbot.make_request(msg_content)
-
         else:
             print(f"Invalid command option: {e}")
             return None
+        
+        print(msg_content)
+        response = chatbot.make_request(msg_content)
+        print(response)
+        return response
 
 # Script that takes command and option and then execute and returns results back to the users. 
 if __name__ == "__main__":
-    # base_url = "https://api.example.com"  # Replace with the actual API base URL
     client = CopilotClient()
     print(len(sys.argv))
 
     if len(sys.argv) != 4 and len(sys.argv) != 5:
-        print("Usage: python script.py <command> <command_content> <option_content>")
+        print("Usage: python script.py <sys_command> <sys_command_output> <capy_command> <question (optional)>")
         sys.exit(1)
 
-    command = sys.argv[1]
-    command_result = sys.argv[2]
-    option_content = sys.argv[3]
-    question = None
-    if len(sys.argv) == 5:
-        question = sys.argv[4]
-    print(command, command_result, option_content)
+    sys_command = sys.argv[1]
+    sys_command_output = sys.argv[2]
+    capy_command = sys.argv[3]
+    question = sys.argv[4] if len(sys.argv) == 5 else None
+    print(sys_command, sys_command_output, capy_command)
 
     # This is a test and here is the test instruction
     # execute python capy_api.py "tf plan" "test" "explain"
     # execute python capy_api.py "tf plan" "test" "cost"
     # execute python capy_api.py "tf plan" "test" "question" "what does this tf plan do?"
-    test_tf_output = """
-    Terraform will perform the following actions:
+    sys_output = ""
+    if sys_command_output == "test":
+        sys_output = """
+            Terraform will perform the following actions:
 
-      # azurerm_resource_group.tf-plan will be created
-      + resource "azurerm_resource_group" "tf-plan" {
-          + id       = (known after apply)
-          + location = "centralus"
-          + name     = "rg-tf-plan-example-centralus"
-          + tags     = {
-              + "name" = "test"
-            }
-        }
+              # azurerm_resource_group.tf-plan will be created
+              + resource "azurerm_resource_group" "tf-plan" {
+                  + id       = (known after apply)
+                  + location = "centralus"
+                  + name     = "rg-tf-plan-example-centralus"
+                  + tags     = {
+                      + "name" = "test"
+                    }
+                }
 
-    Plan: 1 to add, 0 to change, 0 to destroy.
-    """
+            Plan: 1 to add, 0 to change, 0 to destroy.
+            """
+    else:
+        sys_output = sys_command_output
     
-    client.make_request_to_bot(test_tf_output, option_content, question)
+    
+    client.make_request_to_bot(sys_command, sys_output, capy_command, question)
     
     
     
